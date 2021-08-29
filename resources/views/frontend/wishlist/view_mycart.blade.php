87d4c650867aa1491299e0486e1883cffb7383be
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-08-29 23:07:47 +0530

    .

--- resources/views/frontend/wishlist/view_mycart.blade.php
+++ resources/views/frontend/wishlist/view_mycart.blade.php
@@ -37,6 +37,63 @@ My Cart Page
 
                         </div>
                     </div>
+                    <div class="col-12 col-xl-4">
+                        <div class="checkout-form p-3 bg-light">
+                            <div class="card rounded-0 border bg-transparent shadow-none">
+                                <div class="card-body">
+                                    <p class="fs-5">Apply Discount Code</p>
+                                    <div class="input-group">
+                                        <input type="text" class="form-control rounded-0" placeholder="Enter discount code">
+                                        <button class="btn btn-dark btn-ecomm" type="button">Apply Discount</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card rounded-0 border bg-transparent shadow-none">
+                                <div class="card-body">
+                                    <p class="fs-5">Estimate Shipping and Tax</p>
+                                    <div class="my-3 border-top"></div>
+                                    <div class="mb-3">
+                                        <label class="form-label">Country Name</label>
+                                        <select class="form-select rounded-0">
+                                            <option selected>United States</option>
+                                            <option value="1">Australia</option>
+                                            <option value="2">India</option>
+                                            <option value="3">Canada</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">State/Province</label>
+                                        <select class="form-select rounded-0">
+                                            <option selected>California</option>
+                                            <option value="1">Texas</option>
+                                            <option value="2">Canada</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-0">
+                                        <label class="form-label">Zip/Postal Code</label>
+                                        <input type="email" class="form-control rounded-0">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card rounded-0 border bg-transparent mb-0 shadow-none">
+                                <div class="card-body">
+                                    <p class="mb-2">Subtotal: <span class="float-end">$198.00</span>
+                                    </p>
+                                    <p class="mb-2">Shipping: <span class="float-end">--</span>
+                                    </p>
+                                    <p class="mb-2">Taxes: <span class="float-end">$14.00</span>
+                                    </p>
+                                    <p class="mb-0">Discount: <span class="float-end">--</span>
+                                    </p>
+                                    <div class="my-3 border-top"></div>
+                                    <h5 class="mb-0">Order Total: <span class="float-end">212.00</span></h5>
+                                    <div class="my-4"></div>
+                                    <div class="d-grid"> <a href="javascript:;" class="btn btn-dark btn-ecomm">Proceed to Checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
