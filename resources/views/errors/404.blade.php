d6fb754f0c8e7f39401f175270d19a106c01b077
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-09-13 03:10:21 +0530

    404 page created for url error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('frontend.main_master')
+@section('content')
+
+<div class="body-content">
+    <div class="container">
+        <div class="x-page inner-bottom-sm">
+            <div class="row">
+                <div class="col-md-12 x-text text-center">
+                    <h1>404</h1>
+                    <h6>We are sorry, the page you've requested is not available. </h6>
+                    <a href="{{url('/')}}"><i class="fa fa-home"></i> Go To Homepage</a>
+                    <br><br><br>
+                    <br><br><br>
+                    <br><br><br>
+                </div>
+
+            </div><!-- /.row -->
+        </div><!-- /.sigin-in-->
+    </div><!-- /.container -->
+</div><!-- /.body-content -->
+@endsection
