bf011cb8101c1fdea8c42eca021d09c2ba7a8351
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-09-14 09:28:53 +0530

    404 page modified

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,11 +6,13 @@
         <div class="x-page inner-bottom-sm">
             <div class="row">
                 <div class="col-md-12 x-text text-center">
-                    <h1>404</h1>
-                    <h6>We are sorry, the page you've requested is not available. </h6>
-                    <a href="{{url('/')}}"><i class="fa fa-home"></i> Go To Homepage</a>
                     <br><br><br>
-                    <br><br><br>
+                    <div class="error-body text-center">
+                        <h1>404</h1>
+                        <h3 class="text-uppercase">Page Not Found !</h3>
+                        <p class="text-muted m-t-30 m-b-30">YOU SEEM TO BE TRYING TO FIND HIS WAY HOME</p>
+                        <a href="{{url('/')}}" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a>
+                    </div>
                     <br><br><br>
                 </div>
 
