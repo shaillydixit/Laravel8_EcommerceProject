4c0235c47e8ee0bb2c55af4ec92de3d90c02620a
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-09-21 13:12:34 +0530

    slider functionality added

--- resources/views/frontend/body/slider.blade.php
+++ resources/views/frontend/body/slider.blade.php
@@ -6,55 +6,48 @@
                 <li data-bs-target="#carouselExampleDark" data-bs-slide-to="1"></li>
                 <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
             </ol>
+            @php
+            $sliders = App\Models\Slider::where('status', 1)->orderBy('id', 'ASC')->get();
+            @endphp
+            @foreach($sliders as $slider )
             <div class="carousel-inner">
                 <div class="carousel-item active bg-dark-gery">
                     <div class="row d-flex align-items-center">
                         <div class="col d-none d-lg-flex justify-content-center">
                             <div class="">
-                                <h3 class="h3 fw-light">Has just arrived!</h3>
-                                <h1 class="h1">Huge Summer Collection</h1>
-                                <p class="pb-3">Swimwear, Tops, Shorts, Sunglasses &amp; much more...</p>
-                                <div class=""> <a class="btn btn-dark btn-ecomm" href="javascript:;">Shop Now <i class='bx bx-chevron-right'></i></a>
+                                <h1 class="h1">{{$slider->title}}</h1>
+                                <p class="pb-3">{{$slider->description}}</p>
+                                <div class=""> <a class="btn btn-dark btn-ecomm" href="{{route('shop.page')}}">Shop Now <i class='bx bx-chevron-right'></i></a>
                                 </div>
                             </div>
                         </div>
                         <div class="col">
-                            <img src="{{asset('frontend/assets/images/slider/04.png')}}" class="img-fluid" alt="...">
+                            <img src="{{asset($slider->slider_image)}}" class="img-fluid" alt="...">
                         </div>
                     </div>
                 </div>
+                @endforeach
+
+                @php
+                $sliders = App\Models\Slider::where('status', 0)->orderBy('id', 'DESC')->get();
+                @endphp
+                @foreach($sliders as $slider )
                 <div class="carousel-item bg-dark-gery">
                     <div class="row d-flex align-items-center">
                         <div class="col d-none d-lg-flex justify-content-center">
                             <div class="">
-                                <h3 class="h3 fw-light">Hurry up! Limited time offer.</h3>
-                                <h1 class="h1">Women Sportswear Sale</h1>
-                                <p class="pb-3">Sneakers, Keds, Sweatshirts, Hoodies &amp; much more...</p>
-                                <div class=""> <a class="btn btn-dark btn-ecomm" href="javascript:;">Shop Now <i class='bx bx-chevron-right'></i></a>
+                                <h1 class="h1">{{$slider->title}}</h1>
+                                <p class="pb-3">{{$slider->description}}</p>
+                                <div class=""> <a class="btn btn-dark btn-ecomm" href="{{route('shop.page')}}">Shop Now <i class='bx bx-chevron-right'></i></a>
                                 </div>
                             </div>
                         </div>
                         <div class="col">
-                            <img src="{{asset('frontend/assets/images/slider/05.png')}}" class="img-fluid" alt="...">
+                            <img src="{{asset($slider->slider_image)}}" class="img-fluid" alt="...">
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item bg-dark-gery">
-                    <div class="row d-flex align-items-center">
-                        <div class="col d-none d-lg-flex justify-content-center">
-                            <div class="">
-                                <h3 class="h3 fw-light">Complete your look with</h3>
-                                <h1 class="h1">New Men's Accessories</h1>
-                                <p class="pb-3">Hats &amp; Caps, Sunglasses, Bags &amp; much more...</p>
-                                <div class=""> <a class="btn btn-dark btn-ecomm" href="javascript:;">Shop Now <i class='bx bx-chevron-right'></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <img src="{{asset('frontend/assets/images/slider/03.png')}}" class="img-fluid" alt="...">
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
             <a class="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
