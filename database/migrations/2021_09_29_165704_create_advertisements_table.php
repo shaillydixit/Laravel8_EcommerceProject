5dd49808cc440c3f676cd5b8c088245d80dd9cb5
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-09-29 23:00:04 +0530

    advertise model and acontroller created

---
+++ database/migrations/2021_09_29_165704_create_advertisements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertisementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+            $table->string('ads_image');
+            $table->integer('ads_discount');
+            $table->string('ads_title');
+            $table->string('ads_description');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisements');
+    }
+}
